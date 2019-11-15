cd8d19841053bc4fda6ef9bd376b59a3ca5a36ce
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-11-15 12:13:14 +0200

    intial project changes to all folder files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Header from "./components/Header.js";
+import { Route } from 'react-router-dom';
 
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components/CharacterCard.js";
 
 export default function App() {
-  return (
-    <main>
-      <Header />
-    </main>
-  );
+
+ return (
+   
+   <main>
+     <Header />
+     <Route exact path='/' component={WelcomePage}/>
+     <Route  path='/' component={CharacterCard}/>
+    <Route  path='/characters'component={CharacterList}/>
+   </main>
+ );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard({character}) {
+  return (
+    <div>
+      <h2>Name: {character.name}</h2>
+      <p>Gender:{character.gender}</p>
+      <p>Species:{character.species}</p>
+      <p>Location:{character.location}</p>
+    </div>
+  );
 }
+ 
+  
