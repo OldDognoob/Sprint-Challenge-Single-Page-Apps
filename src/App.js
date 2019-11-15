89c45d33e94c857fe73db72b526081efacb197bd
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-11-15 14:35:18 +0200

    display data character

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Header from "./components/Header.js";
+import {Route} from 'react-router-dom';
 
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components/CharacterCard"
+
+ 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path='/' component={WelcomePage}/>
+      <Route path='/characters' component={CharacterList}/> 
     </main>
   );
 }
